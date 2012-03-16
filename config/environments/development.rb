4cfae7a5640467bb486db0b2da462f46ff5de2fb
Info xyz <xyz@xyz-HuronRiver-Platform.(none)>
2012-03-16 19:11:17 -0300

    Funcionando envio de confirmação de email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,21 +14,21 @@ Fitinaplace::Application.configure do
   config.action_controller.perform_caching = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'gmail.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   # change to false to prevent email from being sent during development
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "labarq.usp.br",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "fitinaplace",
+    password: "fitinaplace12345"
   }
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,21 +61,21 @@ Fitinaplace::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'usp.br' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "labarq.usp.br",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "fitinaplace@gmail.com",
+    user_name: "fitinaplace",
     password: "fitinaplace12345"
   }
 
