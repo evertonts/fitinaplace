66739afdfa1ebbc66b58e7061302b2cfb67dcb86
Info xyz <xyz@xyz-HuronRiver-Platform.(none)>
2012-03-16 14:33:29 -0300

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitinaplace::Application.config.secret_token = '9fa845befbc7f21cfea3f1c8b58271281664f37615f43b53f05435d1f248de7edec92b69521b9b9d3fab694fafe88218c476091a31757947295727032f8b0b7f'
