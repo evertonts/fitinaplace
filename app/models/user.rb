a4f0d9a05ceab08cf84af8a2a9064a805a978360
Info xyz <xyz@xyz-HuronRiver-Platform.(none)>
2012-03-23 13:38:42 -0300

    Criada a tela de usuarios e alteracao no model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   validates_presence_of :rg, :email, :cpf, :telefone,:name
-  validates_uniqueness_of :rg, :email, :cpf, :telefone, :case_sensitive => false
+  validates_uniqueness_of  :email, :cpf,  :case_sensitive => false
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :rg, :email, :cpf, :telefone,:name, :email, :password, :password_confirmation, :remember_me, :confirmed_at
