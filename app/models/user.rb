e86aba34d41beb19018a1bf2778f1f2fd73ccbdc
Your Name <everton2x4@gmail.com>
2012-03-30 14:17:06 -0300

    Usuario já pode cadastrar um avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   validates_presence_of :rg, :email, :cpf, :telefone,:name
   validates_uniqueness_of  :email, :cpf,  :case_sensitive => false
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
    
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :rg, :email, :cpf, :telefone,:name, :email, :password, :password_confirmation, :remember_me, :confirmed_at
+  attr_accessible :rg, :email, :cpf, :telefone,:name, :email, :password, :password_confirmation, :remember_me, :confirmed_at, :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
