2a959279fb8629903db2b177e18b9188dbf41d0c
Info xyz <xyz@xyz-HuronRiver-Platform.(none)>
2012-07-27 13:48:00 -0300

    Avaliação de usuarios usando star rating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,14 @@ class UsersController < ApplicationController
     @negociations = Negociation.find_all_by_user_id current_user.id
     @comment = Comment.new
     @comments = Comment.find_all_by_user_id @user.id
+    @avg = 0.0
+    i = 0.0
+    @comments.each do |c|
+      unless c.grade == 0 or c.grade.nil?
+        @avg += c.grade
+        i += 1
+      end
+    end
+    @avg = (@avg/i).round unless i == 0.0
   end
 end
